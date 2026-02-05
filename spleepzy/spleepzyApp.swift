d753d8352a91641f2884ec1527d0c3580260a6a3
Saadi Dalloul <73227093+SaadiDalloul@users.noreply.github.com>
2026-02-05 20:16:48 +0200

    Initial Commit

---
+++ spleepzy/spleepzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  spleepzyApp.swift
+//  spleepzy
+//
+//  Created by Saadi Dalloul on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct spleepzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
